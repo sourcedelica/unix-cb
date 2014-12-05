8ee40ba1cb95796148d3d22fd682686fcc5a81c2
Gary Grossman <gary@explory.com>
2014-12-04 22:22:53 -0800

    Pluggable storage backends for cb_server, including file-based. STORAGE_ENGINE environment variable can now be set to "dummy", "file" or "redis". Default is "file". In "dummy" mode, user records are stored in memory and not persisted. In "file" mode, a .json file is written for each user into the lib/cb/recs directory. In "redis" mode, user records are persisted in Redis.

---
+++ cb_server/file_store.js
@@ -0,0 +1,36 @@
+//
+//  file_store.js
+//
+//  Text file data store backend for cb_server
+//
+
+var path = require('path');
+var fs   = require('fs');
+
+var RECS_PATH = process.env.RECS_PATH || path.resolve('../lib/cb/recs');
+
+function userPath(username) {
+  return path.join(RECS_PATH, username + '.json');
+}
+
+module.exports = {
+  getUser: function (username, callback) {
+    fs.readFile(userPath(username), {encoding:'UTF-8'}, function (err, data) {
+      if (err != null) {
+        callback(null, null);
+      } else {
+        callback(null, JSON.parse(data));
+      }
+    });
+  },
+
+  isExistingUser: function (username, callback) {
+    fs.exists(userPath(username), function (exists) {
+      callback(null, exists);
+    });
+  },
+
+  createUser: function (user, callback) {
+    fs.writeFile(userPath(user.username), JSON.stringify(user), callback);
+  }
+}
